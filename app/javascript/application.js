b1f13b58eec25a091faf9c75bc76b690206b02c9
Ionar Rafael Michielin <irm@ionar.com.br>
2023-11-22 16:40:29 -0300

    test disable js function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,18 +4,18 @@ import "controllers"
 
 // console.log("Log do application.js works")
 
-function checkMediaQuery() {
-    const desktopContainer = document.getElementById("form-inscricao");
-    const formulario = document.getElementById("formulario");
-    const celularContainer = document.getElementById("form-celular");
-    // If the inner width of the window is greater then 768px
-    if (window.innerWidth < 992) {
-      // Then log this message to the console
-      celularContainer.appendChild(formulario);
-    } else {
-        desktopContainer.appendChild(formulario)
-    }
-}
-  // Add a listener for when the window resizes
-  window.addEventListener('load', checkMediaQuery);
-  window.addEventListener('resize', checkMediaQuery);
+// function checkMediaQuery() {
+//     const desktopContainer = document.getElementById("form-inscricao");
+//     const formulario = document.getElementById("formulario");
+//     const celularContainer = document.getElementById("form-celular");
+//     // If the inner width of the window is greater then 768px
+//     if (window.innerWidth < 992) {
+//       // Then log this message to the console
+//       celularContainer.appendChild(formulario);
+//     } else {
+//         desktopContainer.appendChild(formulario)
+//     }
+// }
+//   // Add a listener for when the window resizes
+//   window.addEventListener('load', checkMediaQuery);
+//   window.addEventListener('resize', checkMediaQuery);
